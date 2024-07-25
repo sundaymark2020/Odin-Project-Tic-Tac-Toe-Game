8dd52d4cfacf68ca15677f6db48be01608a5b9b5
Sunday Mark <166103886+sundaymark2020@users.noreply.github.com>
2024-07-25 17:22:16 +0100

    created the winning conditions for the game

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 
+const cells = document.querySelectorAll('.cells');
+const gamestatus = document.querySelector(".gamestatus");
+const resetBtn = document.querySelector('.reset');
+//conditions for winning the game//
+const winningconditions = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+]
+
+//each empty array represent each ell//
+
+let optionCell = ['','','','','','','','',''];
+
+
